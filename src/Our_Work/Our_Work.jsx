4c4514099894fb25cfeb1327470207afbb389700
Genesis <genasears248@gmail.com>
2025-12-06 21:47:05 -0500

    Fix image imports for gallery

--- src/Our_Work/Our_Work.jsx
+++ src/Our_Work/Our_Work.jsx
@@ -1,4 +1,10 @@
 import "./Our_Work.css";
+import img1 from "../../images/img1.jpg";
+import img2 from "../../images/img2.jpg";
+import img3 from "../../images/img3.jpg";
+import img4 from "../../images/img4.jpg";
+import img5 from "../../images/img5.jpg";
+import img6 from "../../images/img6.jpg";
 
 export function Our_Work() {
   return (
@@ -10,27 +16,27 @@ export function Our_Work() {
 
       <div className="work_list">
         <div className="work_card">
-          <img src="../../images/img1.jpg" alt="" className="work_img" />
+          <img src={img1} alt="Image of nails" className="work_img" />
         </div>
 
         <div className="work_card">
-          <img src="../../images/img2.jpg" alt="" className="work_img" />
+          <img src={img2} alt="Image of nails" className="work_img" />
         </div>
 
         <div className="work_card">
-          <img src="../../images/img3.jpg" alt="" className="work_img" />
+          <img src={img3} alt="Image of nails" className="work_img" />
         </div>
 
         <div className="work_card">
-          <img src="../../images/img4.jpg" alt="" className="work_img" />
+          <img src={img4} alt="Image of nails" className="work_img" />
         </div>
 
         <div className="work_card">
-          <img src="../../images/img5.jpg" alt="" className="work_img" />
+          <img src={img5} alt="Image of nails" className="work_img" />
         </div>
 
         <div className="work_card">
-          <img src="../../images/img6.jpg" alt="" className="work_img" />
+          <img src={img6} alt="Image of nails" className="work_img" />
         </div>
       </div>
     </section>
