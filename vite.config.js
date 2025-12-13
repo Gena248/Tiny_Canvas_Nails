cf150605fcdafd08552c170abcb941fc98a8fe93
Genesis <genasears248@gmail.com>
2025-12-13 03:18:19 -0500

    Update Vite config to output to docs

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Tiny_Canvas_Nails/", // replace with your repository name
+  build: {
+    outDir: "docs",
+  },
 });
